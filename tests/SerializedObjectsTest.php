fc71a3117ed86100e142afff3b173869bf75d537
Ilya Zelenin <wyster@make.im>
2014-10-20 18:16:51 +0200

    Changes in tests

--- tests/SerializedObjectsTest.php
+++ tests/SerializedObjectsTest.php
@@ -3,6 +3,7 @@
 namespace LightXml\Tests;
 
 use LightXml\SerializedItem;
+use LightXml\SerializedList;
 
 /**
  * @author Ilya Zelenin <wyster@make.im>
@@ -11,11 +12,39 @@ class SerializedObjectsTest extends \PHPUnit_Framework_TestCase
 {
     public function testData()
     {
-        $item = new SerializedItem();
+        $item = new SerializedItem([], 'root');
 
-        $this->assertSame('root', $item->getNodeName());
+        $xml = '<root/>';
+        $this->assertXmlStringEqualsXmlString($xml, $item);
+        $this->assertXmlStringEqualsXmlString($xml, $item->toString());
 
         $item->setNodeName('main');
-        $this->assertSame('main', $item->getNodeName());
+
+        $xml = '<main/>';
+        $this->assertXmlStringEqualsXmlString($xml, $item);
+        $this->assertXmlStringEqualsXmlString($xml, $item->toString());
+
+        $xml = '<item><id>1</id><value>foo</value></item>';
+        $item = new SerializedItem(['item' => ['id' => 1, 'value' => 'foo']], FALSE);
+        $this->assertXmlStringEqualsXmlString($xml, $item);
+        $this->assertXmlStringEqualsXmlString($xml, $item->toString());
+
+        $xml = '<item><id>1</id><value>foo</value></item>';
+        $item = new SerializedItem(['id' => 1, 'value' => 'foo'], 'item');
+        $this->assertXmlStringEqualsXmlString($xml, $item);
+        $this->assertXmlStringEqualsXmlString($xml, $item->toString());
+
+        $xml = '<root><item><id>2</id><value>bar</value></item><item><id>1</id><value>foo</value></item></root>';
+        $item = new SerializedList([
+            new SerializedItem(['id' => 2, 'value' => 'bar'], 'item'),
+            new SerializedItem(['id' => 1, 'value' => 'foo'], 'item')
+        ], 'root');
+        $this->assertXmlStringEqualsXmlString($xml, $item);
+        $this->assertXmlStringEqualsXmlString($xml, $item->toString());
+
+        $xmlString = '<root><id>1</id><value>foo</value></root>';
+        $item = new SerializedItem(['id' => 1, 'value' => 'foo'], 'root');
+        $this->assertXmlStringEqualsXmlString($xmlString, $item);
+        $this->assertXmlStringEqualsXmlString($xmlString, $item->toString());
     }
 }
--- tests/WriterTest.php
+++ tests/WriterTest.php
@@ -196,42 +196,4 @@ XML;
 
         $this->assertXmlStringEqualsXmlString($xml, $serialize->toString($data));
     }
-
-    public function testAbstractSerializedObject()
-    {
-        $item = new SerializedItem([], 'root');
-
-        $xml = '<root/>';
-        $this->assertXmlStringEqualsXmlString($xml, $item);
-        $this->assertXmlStringEqualsXmlString($xml, $item->toString());
-
-        $item->setNodeName('main');
-
-        $xml = '<main/>';
-        $this->assertXmlStringEqualsXmlString($xml, $item);
-        $this->assertXmlStringEqualsXmlString($xml, $item->toString());
-
-        $xml = '<item><id>1</id><value>foo</value></item>';
-        $item = new SerializedItem(['item' => ['id' => 1, 'value' => 'foo']], FALSE);
-        $this->assertXmlStringEqualsXmlString($xml, $item);
-        $this->assertXmlStringEqualsXmlString($xml, $item->toString());
-
-        $xml = '<item><id>1</id><value>foo</value></item>';
-        $item = new SerializedItem(['id' => 1, 'value' => 'foo'], 'item');
-        $this->assertXmlStringEqualsXmlString($xml, $item);
-        $this->assertXmlStringEqualsXmlString($xml, $item->toString());
-
-        $xml = '<root><item><id>2</id><value>bar</value></item><item><id>1</id><value>foo</value></item></root>';
-        $item = new SerializedList([
-            new SerializedItem(['id' => 2, 'value' => 'bar'], 'item'),
-            new SerializedItem(['id' => 1, 'value' => 'foo'], 'item')
-        ], 'root');
-        $this->assertXmlStringEqualsXmlString($xml, $item);
-        $this->assertXmlStringEqualsXmlString($xml, $item->toString());
-
-        $xmlString = '<root><id>1</id><value>foo</value></root>';
-        $item = new SerializedItem(['id' => 1, 'value' => 'foo'], 'root');
-        $this->assertXmlStringEqualsXmlString($xmlString, $item);
-        $this->assertXmlStringEqualsXmlString($xmlString, $item->toString());
-    }
 }
